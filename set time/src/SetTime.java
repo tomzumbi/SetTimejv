17bdb28a9843ef4e1fce4e3014e79562da4b0aeb
tomzumbi <114890330+tomzumbi@users.noreply.github.com>
2022-11-01 15:52:43 +0700

    Add files via upload

---
+++ set time/src/SetTime.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class SetTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("bay gio la " + now);
+    }
+}
